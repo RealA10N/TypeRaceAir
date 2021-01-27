2eefd705e15fca8eefa253db1ad68a8966ba01dc
Alon (PC) <downtown2u@gmail.com>
2021-01-27 13:28:43 +0200

    ✨ BaseDatabase implementation

--- databases.py
+++ databases.py
@@ -19,10 +19,57 @@ The main dababases are:
 
 """
 
+import os
+import atexit
+import json
+
 
 class BaseDatabase:
     """ Basic and general database. The other databases that the bot uses
     are based on this object. """
+
+    DEFAULT_DB = list()
+
+    def __init__(self, db_name: str):
+        """ Loads the database with the given name. """
+
+        self.name = db_name
+        self._data = self.__load()
+        atexit.register(self.save)  # Save databases when program exits
+
+    @property
+    def filename(self,):
+        """ Returns the database filename. """
+        return self.name + '.json'
+
+    @property
+    def path(self,):
+        """ Returns the path to the `.json` file that contains the current
+        database. """
+
+        return os.path.join(os.getcwd(), 'db', self.filename)
+
+    def __load(self,):
+        """ Loads the database from local storage. If database is not found,
+        loads an empty database. """
+
+        if not os.path.isfile(self.path):
+            # If database file does not exist, loads and returns default
+            # database
+            return self.DEFAULT_DB
+
+        # If database found: opens and returns it.
+        with open(self.path, 'r') as file:
+            return json.load(file)
+
+    def save(self,):
+        """ Saves the database that is loaded in the memory to the local
+        storage. """
+
+        os.makedirs(os.path.dirname(self.path), exist_ok=True)
+
+        with open(self.path, 'w') as file:
+            json.dump(self._data, file)
 
 
 class StatsDatabase(BaseDatabase):

081cd3a144d9c364d71d2039dd55aa372ee7d3cf
orik1504 <orik1504@gmail.com>
2021-01-30 01:08:41 +0200

    ✨started working on player and race classes
    started working on player and race classes, fixed linting and finished PlayerRaceResult class

--- TypeRaceAir/race.py
+++ TypeRaceAir/race.py
@@ -1,15 +1,48 @@
+# -- IMPORTS --#
 import typing
+import time
 
 
 class Race():
 
     def __init__(self, sentece: str, players: typing.List[Player]):
+        '''The race sentence'''
         self.sentece = sentece
+        '''	The players that participate in the race.'''
         self.players = players
+        self.start_timestamp = None
+
+    @property
+    def _players(self,):
+        '''A list of players that participate in the current race.'''
+        pass
+
+    @property
+    def _is_over(self,) -> bool:
+        '''checks if the race is over'''
+        pass
+
+    @property
+    def __start_timestamp(self) -> None:
+        ''' return the current timestamp from epoch '''
+        return time.time()
+
+    def log_result(self, player: Player, user_string: str):
+        '''Logs the result of the given player.'''
+        pass
+        # return PlayerRaceResult(X, self.player, self.__start_timestamp, self.user_string)
 
 
 class Player():
-    pass
+    @property
+    def name(self, name: str) -> name:
+        self.name = name
+
+    def __eq__(self, other) -> bool:
+        pass
+
+    def __ne__(self, other):
+        pass
 
 
 class PlayerRaceResult():
@@ -35,29 +68,29 @@ class PlayerRaceResult():
             if cur_submitted == cur_target
         )
 
-    @ property
+    @property
     def accuracy(self,) -> float:
         '''The percentage of words that the user typed correctly, out of the total words in the sentence. '''
         total_words = len(self.race.sentece.split())
         return self.words_correct/total_words
 
-    @ property
+    @property
     def time(self,) -> float:
         '''The time that it took for the user to submit his answer, in seconds. '''
         return self.submit_timestamp - self.race.start_timestamp
 
-    @ property
+    @property
     def wpm(self,) -> float:
         '''The WPM (Words Per Minute) of the current race for the current user. '''
         total_words = len(self.race.sentece.split())
         return (60/self.time)*total_words
 
-    @ property
+    @property
     def cwpm(self,) -> float:
         '''The CWPM (Correct Words Per Minute) of the current race for the current user. '''
         return (60/self.time)*self.words_correct
 
-    @ property
+    @property
     def score(self,) -> int:
         '''The total calculated score of the current player in the current race. '''
-        return
+        return self.words_correct+(self.wpm*self.words_correct/100)
